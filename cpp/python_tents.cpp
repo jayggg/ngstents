c2a89869b4f2be5fc3a577f9c5ce3d18d77dfb7a
Dow Drake <dowdrake@msn.com>
2020-07-06 15:01:35 -0700

    wire in new code

--- cpp/python_tents.cpp
+++ cpp/python_tents.cpp
@@ -1,10 +1,14 @@
 #include "tents.hpp"
+#include "conservationlaw.hpp"
 #include <python_ngstd.hpp>
 
 
 
 // Just for initial proof of concept (we need to move from DG -> MTP)
 extern void ExportSymbolicDG (py::module & m); 
+
+// Just enough for Burgers2D for now
+extern void ExportConsLaw (py::module & m);
 
 // Export tent mesh
 extern void ExportTents(py::module & m);
@@ -14,6 +18,7 @@ PYBIND11_MODULE(tents, m) {
 
   ExportSymbolicDG(m);
   ExportTents(m);
+  ExportConsLaw(m);
 }
 
   
