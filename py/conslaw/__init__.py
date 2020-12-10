0cf3b16211c58df95e5338fbe444b26e368e7d6e
Francisco Orlandini <francisco.orlandini@gmail.com>
2020-12-10 17:21:42 +0100

    Added Wave python wrapper

--- py/conslaw/__init__.py
+++ py/conslaw/__init__.py
@@ -9,5 +9,15 @@ def Burgers(tentslab, order):
     """
     return ConservationLaw(tentslab,"burgers",order)
 
+
+def Wave(tentslab, order):
+    """
+    INPUTS:
+    
+    tentslab : TentSlab
+    order    : int : order of the L2 space
+    """
+    return ConservationLaw(tentslab, "wave", order)
+
 # clean up
 del conslaw
