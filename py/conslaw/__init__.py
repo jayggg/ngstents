8e58db87f11b04cc2602601bd729019bc6304313
Christoph Wintersteiger <c.wintersteiger@gmx.at>
2020-12-11 11:56:32 +0100

    define equation types as python classes

--- py/conslaw/__init__.py
+++ py/conslaw/__init__.py
@@ -1,23 +1,24 @@
 from .conslaw import ConservationLaw
 
-def Burgers(tentslab, order):
-    """
-    INPUTS:
-    
-    tentslab : TentSlab
-    order    : int : order of the L2 space
-    """
-    return ConservationLaw(tentslab,"burgers",order)
+class Burgers(ConservationLaw):
+    def __init__(self, tentslab, order):
+        """
+        INPUTS:
+        
+        tentslab : TentSlab
+        order    : int : order of the L2 space
+        """
+        ConservationLaw.__init__(self, tentslab, "burgers", order)
 
-
-def Wave(tentslab, order):
-    """
-    INPUTS:
-    
-    tentslab : TentSlab
-    order    : int : order of the L2 space
-    """
-    return ConservationLaw(tentslab, "wave", order)
+class Wave(ConservationLaw):
+    def __init__(self, tentslab, order):
+        """
+        INPUTS:
+        
+        tentslab : TentSlab
+        order    : int : order of the L2 space
+        """
+        ConservationLaw.__init__(self, tentslab, "wave", order)
 
 # clean up
 del conslaw
