44e5fa7e015e59b9bdd34e534f9c36045eebb81d
Christoph Wintersteiger <c.wintersteiger@gmx.at>
2020-12-17 15:53:00 +0100

    move examples in demo folder

--- demo/wave/wave2d.py
+++ demo/wave/wave2d.py
@@ -1,4 +1,4 @@
-from netgen.csg import CSGeometry, OrthoBrick, Pnt
+from netgen.geom2d import SplineGeometry
 from ngsolve import Mesh, Draw, Redraw
 from ngsolve import CoefficientFunction, sqrt, sin, cos, x, y, z
 from ngsolve import TaskManager
@@ -9,15 +9,14 @@ from math import pi
 import time
 
 
-geom = CSGeometry()
-brick = OrthoBrick(Pnt(0, 0, 0), Pnt(pi, pi, pi)).bc(2)
-geom.Add(brick)
+geom = SplineGeometry()
+geom.AddRectangle(p1=(0,0),p2=(pi,pi),bc=2)
 mesh = geom.GenerateMesh(maxh=0.25)
 mesh = Mesh(mesh)
 
 
 # setting the problem
-period = sqrt(3)*pi
+period = sqrt(2)*pi
 n_periods = 1
 t_end = period*n_periods
 dt = t_end/10
@@ -26,7 +25,7 @@ wavespeed = 1
 
 # using causality constant
 local_ctau = True
-global_ctau = 1/1.2
+global_ctau = 2/3
 ts = TentSlab(mesh, method="edge", heapsize=heapsize)
 ts.SetWavespeed(wavespeed)
 ts.PitchTents(dt=dt, local_ct=local_ctau, global_ct=global_ctau)
@@ -37,20 +36,16 @@ print("n tents", ts.GetNTents())
 order = 2
 wave = Wave(ts, order)
 
-p = CoefficientFunction(sin(x)*sin(y)*sin(z))
-u = CoefficientFunction((0, 0, 0))
+p = CoefficientFunction(cos(x)*cos(y))
+u = CoefficientFunction((0,0))
 cf = CoefficientFunction((u, p))
 wave.SetInitial(cf)
 sol = wave.sol
-Draw(sol, sd=5)
+Draw(sol)
 visoptions.scalfunction = "u:4"
-viewoptions.clipping.enable = 1
-visoptions.clipsolution = 'scal'
-
 
 t = 0
 cnt = 0
-# cl.DrawTents()
 
 redraw = 1
 input('start')
@@ -58,16 +53,18 @@ t1 = time.time()
 with TaskManager():
     print("starting...")
     while t < t_end - dt/2:
-        wave.PropagateSARK(sol.vec, substeps=order*order*2)
+        wave.PropagateSAT(sol.vec, stages=order+1, substeps=4*order)
         t += dt
         cnt += 1
         if cnt % redraw == 0:
             print("{:.3f}".format(t))
             Redraw(True)
 print("total time = {}".format(time.time()-t1))
+
 # exact solution
-exsol = CoefficientFunction((-cos(x)*sin(y)*sin(z)*sin(sqrt(3)*t_end)/sqrt(3),
-                             -sin(x)*cos(y)*sin(z)*sin(sqrt(3)*t_end)/sqrt(3),
-                             -sin(x)*cos(y)*cos(z)*sin(sqrt(3)*t_end)/sqrt(3),
-                             sin(x)*sin(y)*sin(z)*cos(sqrt(3)*t_end)))
+exsol = CoefficientFunction((sin(x)*cos(y)*sin(sqrt(2)*t_end)/sqrt(2),
+                             cos(x)*sin(y)*sin(sqrt(2)*t_end)/sqrt(2),
+                             cos(x)*cos(y)*cos(sqrt(2)*t_end)))
 Draw(exsol, mesh, 'exact')
+l2error = sqrt(Integrate(InnerProduct(sol-exsol,sol-exsol),mesh,order=3*order))
+print("l2error = ",l2error)
--- demo/wave/wave3d.py
+++ demo/wave/wave3d.py
@@ -1,4 +1,4 @@
-from netgen.geom2d import SplineGeometry
+from netgen.csg import CSGeometry, OrthoBrick, Pnt
 from ngsolve import Mesh, Draw, Redraw
 from ngsolve import CoefficientFunction, sqrt, sin, cos, x, y, z
 from ngsolve import TaskManager
@@ -9,23 +9,24 @@ from math import pi
 import time
 
 
-geom = SplineGeometry()
-geom.AddRectangle(p1=(0,0),p2=(pi,pi),bc=2)
-mesh = geom.GenerateMesh(maxh=0.25)
+geom = CSGeometry()
+brick = OrthoBrick(Pnt(0, 0, 0), Pnt(pi, pi, pi)).bc(2)
+geom.Add(brick)
+mesh = geom.GenerateMesh(maxh=0.5)
 mesh = Mesh(mesh)
 
 
 # setting the problem
-period = sqrt(2)*pi
+period = sqrt(3)*pi
 n_periods = 1
 t_end = period*n_periods
-dt = t_end/10
+dt = t_end/12
 heapsize = 10*1000*1000
 wavespeed = 1
 
 # using causality constant
 local_ctau = True
-global_ctau = 2/3
+global_ctau = 1/2
 ts = TentSlab(mesh, method="edge", heapsize=heapsize)
 ts.SetWavespeed(wavespeed)
 ts.PitchTents(dt=dt, local_ct=local_ctau, global_ct=global_ctau)
@@ -36,16 +37,20 @@ print("n tents", ts.GetNTents())
 order = 2
 wave = Wave(ts, order)
 
-p = CoefficientFunction(cos(x)*cos(y))
-u = CoefficientFunction((0,0))
+p = CoefficientFunction(cos(x)*cos(y)*cos(z))
+u = CoefficientFunction((0, 0, 0))
 cf = CoefficientFunction((u, p))
 wave.SetInitial(cf)
 sol = wave.sol
-Draw(sol)
+Draw(sol, sd=5)
 visoptions.scalfunction = "u:4"
+viewoptions.clipping.enable = 1
+visoptions.clipsolution = 'scal'
+viewoptions.clipping.dist = 0.5
 
 t = 0
 cnt = 0
+# cl.DrawTents()
 
 redraw = 1
 input('start')
@@ -53,18 +58,18 @@ t1 = time.time()
 with TaskManager():
     print("starting...")
     while t < t_end - dt/2:
-        wave.PropagateSAT(sol.vec, stages=order+1, substeps=order*order*2)
+        wave.PropagateSARK(sol.vec, substeps=order*order)
         t += dt
         cnt += 1
         if cnt % redraw == 0:
             print("{:.3f}".format(t))
             Redraw(True)
 print("total time = {}".format(time.time()-t1))
-
 # exact solution
-exsol = CoefficientFunction((sin(x)*cos(y)*sin(sqrt(2)*t_end)/sqrt(2),
-                             cos(x)*sin(y)*sin(sqrt(2)*t_end)/sqrt(2),
-                             cos(x)*cos(y)*cos(sqrt(2)*t_end)))
+exsol = CoefficientFunction((sin(x)*cos(y)*cos(z)*sin(sqrt(3)*t_end)/sqrt(3),
+                             cos(x)*sin(y)*cos(z)*sin(sqrt(3)*t_end)/sqrt(3),
+                             cos(x)*cos(y)*sin(z)*sin(sqrt(3)*t_end)/sqrt(3),
+                             cos(x)*cos(y)*cos(z)*cos(sqrt(3)*t_end)))
 Draw(exsol, mesh, 'exact')
 l2error = sqrt(Integrate(InnerProduct(sol-exsol,sol-exsol),mesh,order=3*order))
 print("l2error = ",l2error)
