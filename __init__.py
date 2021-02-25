3fdef453e5af78ef2c1910ffbb6fb34c587cf427
Christoph Wintersteiger <c.wintersteiger@gmx.at>
2021-02-25 14:16:26 +0100

    remove old init file

--- __init__.py
+++
@@ -1,5 +0,0 @@
-__all__ = ["TentSlab", "Burgers", "Make1DMesh", "Make1DMeshSimple",
-           "Make1DMeshSpecified", "Make1DPeriodicMesh"]
-
-from .py import (TentSlab, Burgers, Make1DMeshSimple, Make1DMeshSpecified,
-                 Make1DMesh, Make1DPeriodicMesh)
