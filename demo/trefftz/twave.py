583fdb06af67e5a2398f564823f96ee669c61d45
Paul <stocker.paul@hotmail.com>
2021-08-18 17:54:28 +0200

    add ngstrefftz project

---
+++ demo/trefftz/twave.py
@@ -0,0 +1,62 @@
+from ngsolve.internal import visoptions
+from ngstents import TentSlab
+from ngstrefftz import *
+from ngsolve.TensorProductTools import *
+from ngsolve import *
+import time
+from math import pi
+
+
+geom = SplineGeometry()
+geom.AddRectangle(p1=(0, 0), p2=(pi, pi), bc="neumann")
+mesh = Mesh(geom.GenerateMesh(maxh=0.25))
+
+# setting the problem
+period = sqrt(2)*pi
+n_periods = 1
+t_end = period*n_periods
+dt = t_end/10
+heapsize = 10*1000*1000
+wavespeed = 1
+
+# using causality constant
+local_ctau = True
+global_ctau = 2/3
+ts = TentSlab(mesh, method="edge", heapsize=heapsize)
+ts.SetMaxWavespeed(wavespeed)
+ts.PitchTents(dt=dt, local_ct=local_ctau, global_ct=global_ctau)
+
+order=5
+mu0 = CoefficientFunction(cos(x)*cos(y))
+q0 = CoefficientFunction((0, 0))
+cf = CoefficientFunction((q0, mu0))
+TT=TWave(order,ts,CoefficientFunction(wavespeed))
+TT.SetInitial(cf)
+TT.SetBoundaryCF(q0)
+
+t1 = time.time()
+t = 0
+cnt = 0
+redraw = 1
+V = L2(mesh,order=order-1)**3
+u = GridFunction(V,"u")
+Draw(u)
+visoptions.scalfunction = "u:3"
+with TaskManager():
+    while t < t_end - dt/2:
+        TT.Propagate()
+        t += dt
+        # cnt += 1
+        # if cnt % redraw == 0:
+            # print("{:.3f}".format(t))
+            # TT.GetWave(u)
+            # Redraw(True)
+print("total time = {}".format(time.time()-t1))
+
+# exact solution
+exsol = CoefficientFunction((sin(x)*cos(y)*sin(sqrt(2)*t_end)/sqrt(2),
+                             cos(x)*sin(y)*sin(sqrt(2)*t_end)/sqrt(2),
+                             cos(x)*cos(y)*cos(sqrt(2)*t_end)))
+TT.GetWave(u)
+l2error = sqrt(Integrate(InnerProduct(u-exsol, u-exsol), mesh, order=3*order))
+print("l2error = ", l2error)
---
+++ src/trefftz/trefftzfespace.hpp
@@ -0,0 +1,83 @@
+#ifndef FILE_TREFFTZFESPACE_HPP
+#define FILE_TREFFTZFESPACE_HPP
+
+#include "scalarmappedfe.hpp"
+
+namespace ngcomp
+{
+
+    struct GenericSqrt {
+        template <typename T> T operator() (T x) const { return sqrt(x); }
+        static string Name() { return "sqrt"; }
+        void DoArchive(Archive& ar) {}
+    };
+
+    class PolBasis {};
+
+    template<int D>
+    class TWaveBasis : public PolBasis
+    {
+        static void TB_inner(int ord, Matrix<> &trefftzbasis, Vec<D+1, int> coeffnum, int basis, int dim, int &tracker, int basistype, double wavespeed = 1.0);
+        public:
+        TWaveBasis() {;}
+        static CSR Basis(int ord, int basistype = 0, int fosystem = 0);
+        static int IndexMap2(Vec<D+1, int> index, int ord);
+    };
+
+    template<int D>
+    class QTWaveBasis : public PolBasis
+    {
+        mutex gentrefftzbasis;
+        std::map<string,CSR> gtbstore;
+        public:
+        QTWaveBasis() {;}
+        CSR Basis(int ord, Vec<D+1> ElCenter, Matrix<shared_ptr<CoefficientFunction>> GGder, Matrix<shared_ptr<CoefficientFunction>> BBder, double elsize = 1.0, int basistype=0);
+    };
+
+    class TrefftzFESpace : public FESpace
+    {
+        int D;
+        int order;
+        size_t ndof;
+        int nel;
+        int nvert;
+        int local_ndof;
+        float c=1;
+        int useshift=1;
+        int usescale=1;
+        int useqt = 0;
+        int basistype;
+        shared_ptr<CoefficientFunction> wavespeedcf=nullptr;
+        Matrix<shared_ptr<CoefficientFunction>> GGder;
+        Matrix<shared_ptr<CoefficientFunction>> BBder;
+        CSR basismat;
+        PolBasis* basis;
+
+        public:
+            TrefftzFESpace (shared_ptr<MeshAccess> ama, const Flags & flags);
+            void SetWavespeed(shared_ptr<CoefficientFunction> awavespeedcf, shared_ptr<CoefficientFunction> aBBcf = nullptr);
+            string GetClassName () const override { return "trefftz"; }
+            void GetDofNrs (ElementId ei, Array<DofId> & dnums) const override;
+            FiniteElement & GetFE (ElementId ei, Allocator & alloc) const override;
+            size_t GetNDof () const override { return ndof; }
+            static DocInfo GetDocu ();
+
+        protected:
+            template<int D>
+            double Adiam(ElementId ei, double c) const;
+            template<int D>
+            double Adiam(ElementId ei, shared_ptr<CoefficientFunction> c) const;
+            template<int D>
+            Vec<D+1> ElCenter(ElementId ei) const;
+    };
+
+
+
+}
+
+#ifdef NGS_PYTHON
+#include <python_ngstd.hpp>
+void ExportTrefftzFESpace(py::module m);
+#endif // NGS_PYTHON
+
+#endif
