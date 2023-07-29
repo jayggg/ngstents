333dbe53256f862939973dd5936b8cc5c6c10e09
Paul <stocker.paul@hotmail.com>
2023-07-29 17:45:00 +0200

    try windows

--- src/tents.hpp
+++ src/tents.hpp
@@ -89,7 +89,7 @@ public:
   /// height of the tent in the IP's
   Array<FlatVector<SIMD<double>>> adelta;
   /// local numbers of the neighbors
-  Array<INT<2,size_t>> felpos;
+  Array<ngcore::INT<2,size_t>> felpos;
   /// facet integration rules for all facets in the tent
   Array<SIMD_IntegrationRule*> fir;
   /// facet integration rules for all internal facets in the tent
